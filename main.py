452c140121d6c30e3ddb9ed431d1f98910332ab0
alisadaintanvir <alisadaintanvir@gmail.com>
2023-10-31 20:34:38 +0600

    add edit route

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6nzWlSihBXox7C0sKR6b'
@@ -34,7 +35,7 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
@@ -50,9 +51,33 @@ def show_post(index):
     return render_template("post.html", post=requested_post)
 
 
-@app.route('/edit_post')
-def edit_post():
-    return render_template('make-post.html')
+@app.route('/edit_post/<int:index>', methods=['GET', 'POST'])
+def edit_post(index):
+    post = BlogPost.query.get(index)
+    form = CreatePostForm(obj=post)
+    if request.method == 'POST' and form.validate_on_submit():
+        form.populate_obj(post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    return render_template('make-post.html', form=form)
+
+
+@app.route('/new-post', methods=['GET', 'POST'])
+def new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=form.author.data,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template('make-post.html', form=form)
 
 
 @app.route("/about")
